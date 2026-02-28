43260852057ad18b6b7c1b49ad7c9cd81d04cf3a
Demirel1922 <sarper@ormeci-tr.com>
2026-02-28 15:17:54 +0300

    Fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'base: "/OYS-ERP/",
+  base: "/OYS-ERP/",
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
