a852e94700acf8bd3f82760c6727b0b0d01e23ed
Demirel1922 <sarper@ormeci-tr.com>
2026-02-28 15:42:33 +0300

    Fix router basename for Pages

--- src/App.tsx
+++ src/App.tsx
@@ -247,7 +247,7 @@ function AppRoutes() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthWrapper>
         <AppRoutes />
       </AuthWrapper>
